48be10dd478153c6f05121bade74b2f902a1f648
Tobias Riepe <tobias.riepe@protonmail.com>
2021-03-27 23:29:33 +0100

    Create BTC_EUR_chart.py

---
+++ BTC/BTC_EUR_chart.py
@@ -0,0 +1,27 @@
+
+import datetime as dt
+
+import mplfinance as mpf
+import pandas as pd
+import pandas_datareader as web
+import threading
+
+start = dt.datetime(2020, 6, 1)
+end = dt.datetime.now()
+btcusd_data = web.DataReader("BTC-EUR", "yahoo", start, end)
+
+
+##Display
+myscreen_fx = (1)
+myscreen_X = (2560 * myscreen_fx)
+myscreen_Y = (1440 * myscreen_fx)
+dpi_XY = (100)
+
+mpf.plot(btcusd_data,
+         type='candle',
+         figsize=(myscreen_X / dpi_XY, myscreen_Y / dpi_XY),
+         mav=(2, 7, 20),
+         volume=True,
+         style="yahoo",
+         scale_padding=0.3
+         )
